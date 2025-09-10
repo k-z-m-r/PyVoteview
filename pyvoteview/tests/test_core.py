c649c8eecae64aaa8d8aea88bd58d0be0c151149
Jeremy Kazimer <jeremykazimer@gmail.com>
2025-09-09 22:49:52 -0400

    test _validate_chamber()

--- pyvoteview/tests/test_core.py
+++ pyvoteview/tests/test_core.py
@@ -5,12 +5,12 @@ from pytest import raises
 from pyvoteview.core import (
     CURRENT_CONGRESS_NUMBER,
     CURRENT_YEAR,
+    _validate_chamber,
     _validate_congress_number,
 )
 
+
 # _validate_congress_number ----------------------------------------------------
-
-
 def test__validate_congress_number() -> None:
     """Tests that _validate_congress_number works on three cases:
 
@@ -39,3 +39,27 @@ def test__validate_congress_number() -> None:
         rf"{CURRENT_CONGRESS_NUMBER} \({CURRENT_YEAR}\).",
     ):
         _validate_congress_number(CURRENT_CONGRESS_NUMBER + 1)
+
+
+# _validate_chamber -----------------------------------------------------------
+def test__validate_chamber() -> None:
+    """Tests that _validate_chamber() passes on two conditions:
+
+    1. A valid string ("House" or "Senate")
+    2. Raises an error on an invalid string
+    """
+
+    # Case 1
+    _validate_chamber("House")
+    _validate_chamber("Senate")
+
+    # Case 2
+    bad_input = "Supreme Court"
+    with raises(
+        ValueError,
+        match=(
+            "Chamber must be one of House or Senate, "
+            f"but {bad_input} was entered.  The input is case sensitive."
+        ),
+    ):
+        _validate_chamber(bad_input)
